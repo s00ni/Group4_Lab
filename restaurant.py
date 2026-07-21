f7529d2964b468219e0d1945ae972747113a590c
s00ni <haeunsoon@gmail.com>
2026-07-21 12:31:42 -0400

    Implement Restaurant class skeleton
    Added initial structure for Restaurant class with methods for menu display, order management, and total calculation.

--- restaurant.py
+++ restaurant.py
@@ -1,1 +1,16 @@
+from menuItem import MenuItem
+
 class Restaurant: 
+  def __init__(self):
+    self._menu_items = []
+    self._order_list = []
+
+  def display_menu(self):
+  
+  def add_order():
+    
+  def calculate_total():
+    
+  def display_order():
+    
+  def clear_order():
