5668d1c42160912f6384e124e0bda64c6abf5914
s00ni <haeunsoon@gmail.com>
2026-07-21 12:22:13 -0400

    Implement constructor and methods in MenuItem class
    Added constructor and getter methods for MenuItem class.

---
+++ MenuItem.py
@@ -0,0 +1,13 @@
+class MenuItem: 
+  def __init__(self, name, price):
+    self._name = name
+    self._price = price
+
+  def __str__(self):
+    # Fill this in
+
+  def get_name(self):
+    return self._name
+
+  def get_price(self):
+    return self._price
