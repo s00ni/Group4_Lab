e130e6c8fdce75e3cee2dce51f59510a078c436b
s00ni <haeunsoon@gmail.com>
2026-07-22 19:39:01 -0400

    Adding restaurant.py code

--- restaurant.py
+++ restaurant.py
@@ -1,16 +1,25 @@
-from menuItem import MenuItem
-
 class Restaurant: 
   def __init__(self):
-    self._menu_items = []
-    self._order_list = []
+    self.menu_items = []
+    self.order_list = []
 
   def display_menu(self):
+    for i in self.menu_items:
+      print (i)
   
-  def add_order():
+  def add_order(self, menuItem_object):
+    self.order_list.append(menuItem_object)
     
-  def calculate_total():
+  def calculate_total(self):
+    total_price = 0 
+    for item in self.order_list:
+      total_price = total_price + item.price
+    return total_price
+
+  def display_order(self):
+    for i in self.order_list:
+      print (i)
     
-  def display_order():
-    
-  def clear_order():
+  def clear_order(self):
+    self.order_list.clear()
+
